1228ba7d48d1f4076ac1d558f823073d13cbe158
bruno <delgado.hebru24@triana.salesianos.edu>
2023-11-23 21:03:48 +0100

    10 casicasi

--- EjerciciosTema3Parte1BrunoDelgado/src/ejercicio10/Maquina.java
+++ EjerciciosTema3Parte1BrunoDelgado/src/ejercicio10/Maquina.java
@@ -48,11 +48,13 @@ public class Maquina {
 		 System.out.println("*************************************************************");
 			System.out.println("Precio\t\tCantidad\t\tTotal");
 			System.out.println(precioUnitario+"\t\t"+cantidadBilletes+"\t\t\t"+calcularCoste(precioUnitario, cantidadBilletes));
-			System.out.println("-------------");
+			System.out.println("-----------------------");
 			System.out.println("Pagado: ");
 			System.out.print(pagado);
 			System.out.println("\nCambio: ");
 			System.out.print(hacerCambio(pagado, precioUnitario, cantidadBilletes));
+			System.out.println("-----------------------");
+			System.out.println("Mensaje válido para "+cantidadBilletes+" personas");
 			System.out.println("\n*************************************************************");
 	 }
 	 
@@ -60,4 +62,23 @@ public class Maquina {
 	 public void hacerSaldoTotal() {
 			System.out.println(precioTotal);
 		}
+	 
+	 public boolean comprobarContrasena(String contrasena, String leerContrasena) {
+		 
+		 if(leerContrasena==contrasena) {
+			 return true;
+		 }else {
+			 return false;
+		 }
+	 }
+	 public int verComprob(String contrasena, String leerContrasena) {
+		 if (comprobarContrasena(contrasena, leerContrasena)==true) {
+			 int c = 1;
+			 return c;
+		 }else {
+			 int c = 2;
+			 return c;
+		 }
+		 
+	 }
 }
--- EjerciciosTema3Parte1BrunoDelgado/src/ejercicio10/Principal.java
+++ EjerciciosTema3Parte1BrunoDelgado/src/ejercicio10/Principal.java
@@ -6,48 +6,100 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String contrasena = "1234";
+		String leerContrasena;
 		double precioUnitario=0;
-		int cantidadBilletes;
+		int cantidadBilletes = 0;
 		int ticketsComprados=0;
 		double precioTotal=0;
-		double pagado;
+		double pagado = 0;
+		int eleccion;
 		
 		Ticket t = new Ticket(precioUnitario);
 		Maquina m = new Maquina(precioUnitario, precioTotal);
 		
-		System.out.println("Establecer precio ticket");
-		precioUnitario=Leer.datoDouble();
-		precioUnitario=m.establecerPrecio(precioUnitario);
+		
+		System.out.println("Bienvenido, introduce la contraseña para entrar al sistema");
+		System.out.print("Contraseña: ");
+		leerContrasena=Leer.dato();
+		if(m.verComprob(contrasena, leerContrasena)==1) {
+			System.out.println("Bienvenido...");
+			System.out.println();
+			do {
+				System.out.println("""
+						1 para establecer el precio de los billetes
+						2 para compra de billete/s
+						3 para imprimir el ticket
+						4 para ver saldo total
+						0 para salir
+						""");
+				eleccion=Leer.datoInt();
+				switch(eleccion) {
+				case 1:
+					System.out.println("Introduzca precio para el ticket");
+					precioUnitario=Leer.datoDouble();
+					precioUnitario=m.establecerPrecio(precioUnitario);
+					
+
+					break;
+				
+				case 2:
+					System.out.println("¿Cuántos billetes?");
+					cantidadBilletes=Leer.datoInt();
+					ticketsComprados=m.comprarBilletes(cantidadBilletes, precioUnitario, ticketsComprados);
+					System.out.println("El precio es ");
+					m.verCoste(precioUnitario, cantidadBilletes);
+					
+					System.out.println("¿Ccon cuánto han pagado?");
+					pagado=Leer.datoDouble();
+					 
+					if(pagado<m.calcularCoste(precioUnitario, cantidadBilletes)) {
+						System.out.println("No te alcanza");
+					}else {
+						System.out.println("Tu cambio es de:");
+						System.out.println(m.hacerCambio(pagado, precioUnitario, cantidadBilletes));
+					}
+					break;
+					
+				case 3:
+					System.out.println("Imprimiendo ticket...");
+					if(pagado<m.calcularCoste(precioUnitario, cantidadBilletes)) {
+						System.out.println("No te alcanza");
+					}else {
+						m.imprimirTicket(precioUnitario, cantidadBilletes, pagado);
+					}
+				break;
+				
+				case 4:
+					System.out.println("Saldo total:");
+					m.hacerSaldoTotal();
+				break;
+				
+				case 0:
+					System.out.println("Adiós");
+				break;
+				default:
+					System.out.println("Introduce una opción válida");
+				}
+			}while(eleccion!=0);
+		}else {
+			System.out.println("Contraseña incorrecta");
+		}
+		
+		
+
 
 		
-		System.out.println("Comprar billetes");
-		System.out.println("¿Cuántos?");
-		cantidadBilletes=Leer.datoInt();
-		ticketsComprados=m.comprarBilletes(cantidadBilletes, precioUnitario, ticketsComprados);
+	
 		
-		System.out.println("El precio es ");
-		m.verCoste(precioUnitario, cantidadBilletes);
 		
-		System.out.println("¿Ccon cuánto han pagado?");
-		pagado=Leer.datoDouble();
-		 
-		if(pagado<m.calcularCoste(precioUnitario, cantidadBilletes)) {
-			System.out.println("No te alcanza");
-		}else {
-			System.out.println("Tu cambio es de:");
-			System.out.println(m.hacerCambio(pagado, precioUnitario, cantidadBilletes));
-		}
 		
-		System.out.println("Imprimiendo ticket...");
-		if(pagado<m.calcularCoste(precioUnitario, cantidadBilletes)) {
-			System.out.println("No te alcanza");
-		}else {
-			m.imprimirTicket(precioUnitario, cantidadBilletes, pagado);
-		}
 		
-		System.out.println("Saldo total:");
-		m.hacerSaldoTotal();
+		
+		
+		
+		
+		
 		
 
 		
